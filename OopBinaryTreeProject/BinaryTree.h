7b18f90642e7a158bc3b8f635ca1ffbd4cf2472b
Max Efimov <efimov.top.academy@gmail.com>
2025-02-18 11:55:33 +0300

    18-02-25

--- OopBinaryTreeProject/BinaryTree.h
+++ OopBinaryTreeProject/BinaryTree.h
@@ -21,11 +21,11 @@ public:
 
 	Node<T>* CreateNode(Node<T>* parent, T value);
 
-	InsertLoop(T value);
-	InsertReq(T value);
+	void InsertLoop(T value);
+	void InsertReq(T value, Node<T>* node = nullptr);
 
-	RemoveNode(Node<T>* node);
-	RemoveBranch(Node<T>* node);
+	void RemoveNode(Node<T>* node);
+	void RemoveBranch(Node<T>* node);
 
 	Node<T>* Min(Node<T>* node = nullptr);
 	Node<T>* Max(Node<T>* node = nullptr);
@@ -56,6 +56,92 @@ inline BinaryTree<T>::Node<T>* BinaryTree<T>::CreateNode(Node<T>* parent, T valu
 }
 
 template<typename T>
+void BinaryTree<T>::InsertLoop(T value)
+{
+	if (!root)
+	{
+		root = CreateNode(nullptr, value);
+		count = 1;
+		return;
+	}
+
+	Node<T>* nodeCurrent = root;
+	Node<T>* nodeParent = nullptr;
+
+	while (nodeCurrent)
+	{
+		nodeParent = nodeCurrent;
+		if (value < nodeCurrent->value)
+			nodeCurrent = nodeCurrent->left;
+		else
+			nodeCurrent = nodeCurrent->right;
+	}
+
+	nodeCurrent = CreateNode(nodeParent, value);
+	if (nodeCurrent->value < nodeParent->value)
+		nodeParent->left = nodeCurrent;
+	else
+		nodeParent->right = nodeCurrent;
+
+	count++;
+}
+
+template<typename T>
+inline void BinaryTree<T>::InsertReq(T value, Node<T>* node)
+{
+	if (!root)
+	{
+		root = CreateNode(nullptr, value);
+		count = 1;
+		return;
+	}
+
+	if (!node)
+		node = root;
+
+	if (value < node->value)
+	{
+		if (node->left)
+			InsertReq(value, node->left);
+		else
+		{
+			node->left = CreateNode(node, value);
+			count++;
+		}
+			
+	}
+	else
+	{
+		if (node->right)
+			InsertReq(value, node->right);
+		else
+		{
+			node->right = CreateNode(node, value);
+			count++;
+		}	
+	}
+}
+
+template<typename T>
+inline void BinaryTree<T>::RemoveBranch(Node<T>* node)
+{
+	if (node->left)
+		RemoveBranch(node->left);
+	if (node->right)
+		RemoveBranch(node->right);
+
+	if (node != root)
+	{
+		if (node->parent->left == node)
+			node->parent->left = nullptr;
+		else
+			node->parent->right = nullptr;
+	}
+
+	delete node;
+}
+
+template<typename T>
 inline int BinaryTree<T>::Count() const
 {
 	return count;
@@ -65,4 +151,21 @@ template<typename T>
 inline BinaryTree<T>::Node<T>* BinaryTree<T>::Root() const
 {
 	return root;
+}
+
+template<typename T>
+void BinaryTree<T>::PrintBranch(Node<T>* node)
+{
+	if (node)
+	{
+		PrintBranch(node->left);
+		std::cout << node->value << " ";
+		PrintBranch(node->right);
+	}
+}
+
+template<typename T>
+void BinaryTree<T>::Print()
+{
+	PrintBranch(root);
 }
--- OopBinaryTreeProject/OopBinaryTreeProject.cpp
+++ OopBinaryTreeProject/OopBinaryTreeProject.cpp
@@ -1,6 +1,28 @@
 ﻿#include <iostream>
+#include "BinaryTree.h"
 
 int main()
 {
-    
+    BinaryTree<int> tree;
+
+    /*tree.InsertLoop(200);
+    tree.InsertLoop(100);
+    tree.InsertLoop(300);
+    tree.InsertLoop(50);
+    tree.InsertLoop(250);
+    tree.InsertLoop(350);
+    tree.InsertLoop(70);
+    tree.InsertLoop(270);*/
+
+    tree.InsertReq(200);
+    tree.InsertReq(100);
+    tree.InsertReq(300);
+    tree.InsertReq(50);
+    tree.InsertReq(250);
+    tree.InsertReq(350);
+    tree.InsertReq(70);
+    tree.InsertReq(270);
+
+    tree.Print();
+
 }
